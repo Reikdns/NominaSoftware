b116ee84b175f6a54f70c472cacade63c76d67c1
Reikdns <reik.dns@gmail.com>
2023-03-28 02:30:24 -0500

    Estructura de proyecto.

---
+++ Entity/Docente.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entity
+{
+    public class Punto
+    {
+
+        //pregrado
+        public const int PregradoMedicinaHumanaComposicionMusical = 183;
+        public const int Pregrado = 178;
+
+        //posgrado
+        public const int EspecializacionTipoI = 20;
+        public const int EspecializacionTipoII = 30;
+        public const int Magister = 40;
+
+
+
+    }
+}
---
+++ GUI/Formulario.cs
@@ -0,0 +1,10 @@
+namespace GUI
+{
+    public partial class Formulario : Form
+    {
+        public Formulario()
+        {
+            InitializeComponent();
+        }
+    }
+}
