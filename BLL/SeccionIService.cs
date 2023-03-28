7e92b0832265b879f6ba8caf210d35ab38573076
Reikdns <reik.dns@gmail.com>
2023-03-28 03:38:54 -0500

    Seccion 1.

---
+++ BLL/SeccionIService.cs
@@ -0,0 +1,31 @@
+﻿using BLL.Constantes;
+using BLL.Enums;
+namespace BLL
+{
+    public class SeccionIService
+    {
+        public int Puntos { get; set; }
+        public bool EsMedicinaOMusica { get; set; }
+
+        public void CalcularPuntosPorTitulosDePregrado(Titulos titulo)
+        {
+            if(titulo == Titulos.Medicina_Musica)
+            {
+                Puntos += Punto.PregradoMedicinaHumanaComposicionMusical;
+                EsMedicinaOMusica = true;
+                return;
+            }
+
+            Puntos += Punto.Pregrado;
+        }
+
+        public void CalcularPuntosPorTitulosDePosgrado(int? AniosCursados)
+        {
+            if(EsMedicinaOMusica)
+            {
+                
+            }
+        }
+
+    }
+}
--- Entity/Docente.cs
+++ Entity/Docente.cs
@@ -3,19 +3,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Entity
 {
-    public class Punto
+    public class Docente
     {
 
-        //pregrado
-        public const int PregradoMedicinaHumanaComposicionMusical = 183;
-        public const int Pregrado = 178;
-
-        //posgrado
-        public const int EspecializacionTipoI = 20;
-        public const int EspecializacionTipoII = 30;
-        public const int Magister = 40;
-
-
-
+       
     }
 }
