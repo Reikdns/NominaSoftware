992b6de94e9fece0931c6de3f6157436f8740553
Reikdns <reik.dns@gmail.com>
2023-03-28 03:38:54 -0500

    Seccion 1.

---
+++ BLL/Constantes/Punto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Constantes
+{
+    public class Punto
+    {
+
+        //pregrado
+        public const int PregradoMedicinaHumanaComposicionMusical = 183;
+        public const int Pregrado = 178;
+
+        //posgrado
+        public const int EspecializacionTipoI = 20;
+        public const int EspecializacionTipoII = 30;
+
+        public const int MagisterTipoI = 20;
+        public const int MagisterTipoII = 60;
+
+        public const int DoctoradoTipoI = 50;
+        public const int DoctoradoTipoII = 120;
+        public const int DoctoradoTipoIII = 140;
+
+        public const int MedicinaAnio = 15;
+
+
+
+
+
+    }
+}
---
+++ BLL/SeccionIService.cs
@@ -0,0 +1,87 @@
+﻿using BLL.Constantes;
+using BLL.Enums;
+namespace BLL
+{
+    public class SeccionIService
+    {
+        public int Puntos { get; set; }
+        public int PuntosPregrado { get; set; }
+        public int PuntosPosgrado { get; set; }
+        public int PuntosEspecializacion { get; set; }
+        public int PuntosMaestria { get; set; }
+        public int PuntosDoctorado { get; set; }
+        public bool EsMedicinaOMusica { get; set; }
+        public int AniosAcademicosCursados { get; set; }
+        public int NoClinicosProgramasCursados { get; set; }
+        public int MaestriasProgramasCursados { get; set; }
+        public int DoctoradoPtogramasCursados { get; set; }
+
+        public void CalcularPuntosPorTitulosDePregrado(Titulos? titulo)
+        {
+            if(titulo == Titulos.Medicina_Musica)
+            {
+                PuntosPregrado += Punto.PregradoMedicinaHumanaComposicionMusical;
+                EsMedicinaOMusica = true;
+            }
+            else if(titulo == Titulos.OtraArea)
+            {
+                PuntosPregrado += Punto.Pregrado;
+                EsMedicinaOMusica = false;
+            }
+        }
+
+        public void CalcularPuntosPorTitulosDePosgrado(int aniosCursados, int noClinicas, int maestrias, int doctorado)
+        {
+            if (EsMedicinaOMusica)
+            {
+                PuntosEspecializacion += aniosCursados * Punto.MedicinaAnio > 75 ? 75 : aniosCursados * Punto.MedicinaAnio;
+            }
+            else
+            {
+                PuntosEspecializacion += noClinicas > 2 ? noClinicas * Punto.EspecializacionTipoI * 2 : Punto.EspecializacionTipoI;
+            }
+
+            if (maestrias == 1)
+            {
+                PuntosMaestria += Punto.MagisterTipoI;
+            }
+            else if(maestrias > 2)
+            {
+                PuntosMaestria += Punto.MagisterTipoII;
+            }
+
+            if (PuntosEspecializacion + PuntosMaestria >= 60)
+            {
+                PuntosPosgrado = 60;
+            }
+
+            if (doctorado == 1)
+            {
+                PuntosDoctorado += maestrias == 0 ? Punto.DoctoradoTipoII : Punto.DoctoradoTipoI;
+            }
+            else if (doctorado > 2)
+            {
+                PuntosDoctorado += maestrias == 0 ? Punto.DoctoradoTipoIII : Punto.DoctoradoTipoII;
+            }
+
+            if(PuntosPosgrado + PuntosDoctorado > 140)
+            {
+                PuntosPosgrado = 140;
+            }
+        }
+
+        public void Reset()
+        {
+            PuntosPregrado = 0;
+            PuntosDoctorado = 0;
+            PuntosEspecializacion = 0;
+            PuntosMaestria = 0;
+            PuntosPosgrado = 0;
+        }   
+        
+        public int ObtenerPuntos()
+        {
+            return PuntosPosgrado + PuntosPregrado;
+        }
+    }
+}
